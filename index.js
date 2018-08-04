4e89899181dffc0b4759a326ce42a6cf6162456f
kkdd <kkondo_z0@nifty.com>
2018-08-04 11:21:48 +0900

    Update index.js

--- index.js
+++ index.js
@@ -69,8 +69,8 @@ function CheapRuler(lat, units) {
 
     /**
      * Curvature formulas, this.kx = N(lat) and this.ky = M(lat), from
-       https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#Coordinate_system_conversion
-     */    
+     * https://en.wikipedia.org/wiki/Geographic_coordinate_conversion#Coordinate_system_conversion
+     */
     var m = consts.DEGREE * consts.RE * units ? factors[units] : 1;
     var coslat = Math.cos(lat * consts.DEGREE);
     var w2 = 1 / (1 - consts.E2 * (1 - coslat * coslat));
