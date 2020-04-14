521f0dcd2228d4087470cfe1dbeeb69b9fd3e3dc
Denis <carriere.denis@gmail.com>
2020-04-14 16:25:35 -0400

    Improve typescript support

---
+++ cheap-ruler.d.ts
@@ -0,0 +1,37 @@
+export type BBox = [number, number, number, number];
+export type Point = [number, number];
+export type Line = Point[];
+export type Points = Point[];
+export type Polygon = Point[][];
+
+export interface Factors {
+    kilometers: number;
+    miles: number;
+    nauticalmiles: number;
+    meters: number;
+    metres: number;
+    yards: number;
+    feet: number;
+    inches: number;
+}
+
+export default class CheapRuler {
+    public static fromTile(y: number, z: number, units?: string): CheapRuler;
+    public static units: Factors;
+
+    constructor(lat: number, units?: string);
+
+    public distance(a: Point, b: Point): number;
+    public bearing(a: Point, b: Point): number;
+    public destination(p: Point, dist: number, bearing: number): Point;
+    public lineDistance(points: Points): number;
+    public area(polygon: Polygon): number;
+    public along(line: Line, dist: number): Point;
+    public pointOnLine(line: Line, p: Point): {point: Point, index: number, t: number};
+    public lineSlice(start: Point, stop: Point, line: Line): Line;
+    public lineSliceAlong(start: number, stop: number, line: Line): Line;
+    public bufferPoint(p: Point, buffer: number): BBox;
+    public bufferBBox(bbox: BBox, buffer: number): BBox;
+    public insideBBox(p: Point, bbox: BBox): boolean;
+    public offset(p: Point, dx: number, dy: number): Point;
+}
--- test/types.ts
+++ test/types.ts
@@ -1,15 +1,15 @@
-import CheapRuler from '../cheap-ruler.js'
+import CheapRuler, { Point, Line, Polygon } from '../cheap-ruler.js'
 
 /**
  * Declare variables
  */
-const point1 = [30.5, 50.5]
-const point2 = [30.51, 50.49]
-const polygon = [[
+const point1: Point = [30.5, 50.5]
+const point2: Point = [30.51, 50.49]
+const polygon: Polygon = [[
     [-67.031, 50.458], [-67.031, 50.534], [-66.929, 50.534],
     [-66.929, 50.458], [-67.031, 50.458]
 ]]
-const line = [
+const line: Line = [
     [-67.031, 50.458], [-67.031, 50.534],
     [-66.929, 50.534], [-66.929, 50.458]
 ]
@@ -30,3 +30,16 @@ ruler.lineSliceAlong(10, 20, line)
 ruler.bufferPoint(point1, 0.01)
 ruler.bufferBBox([30.5, 50.5, 31, 51], 0.2)
 ruler.insideBBox([30.5, 50.5], [30, 50, 31, 51])
+
+// units
+CheapRuler.units.kilometers
+CheapRuler.units.miles
+CheapRuler.units.nauticalmiles
+CheapRuler.units.meters
+CheapRuler.units.metres
+CheapRuler.units.yards
+CheapRuler.units.feet
+CheapRuler.units.inches
+
+// ruler
+CheapRuler.fromTile(1567, 12);
